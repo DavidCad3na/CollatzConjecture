3996a88df1b799957cd76c15c5948f1adbb680e2
David <Cadenadavid25@gmail.com>
2025-02-17 10:30:59 -0700

    Add project files.

---
+++ collatzConjecture/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace collatzConjecture
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            int fNum = Console.Read();
+
+            if (fNum % 2 == 0)
+            {
+                fNum / 2;
+            }
+
+            else { 
+                fNum * 3;
+
+                fNum + 1;
+            }
+            Console.WriteLine(fNum);
+            return fNum;
+        }
+    }
+}
