4982bc06d16173be25dcd34712305859b7f79df7
David <Cadenadavid25@gmail.com>
2025-02-18 16:49:03 -0700

    Fixed int input so number input is correct, all functionality works correctly

--- collatzConjecture/Program.cs
+++ collatzConjecture/Program.cs
@@ -5,19 +5,21 @@
         static int Main(string[] args)
         {
             Console.WriteLine("Enter Number");
-            int fNum = Console.Read();
-
-            if (fNum % 2 == 0)
+            int fNum = Convert.ToInt32(Console.ReadLine());
+            while (fNum != 1)
             {
-                fNum / 2;
-            }
+                if (fNum % 2 == 0)
+                {
+                    fNum = fNum / 2;
+                    Console.WriteLine(fNum);
+                }
 
-            else { 
-                fNum * 3;
-
-                fNum + 1;
+                else
+                {
+                    fNum = (fNum * 3) + 1;
+                    Console.WriteLine(fNum);
+                }
             }
-            Console.WriteLine(fNum);
             return fNum;
         }
     }
